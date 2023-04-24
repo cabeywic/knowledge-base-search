bd7e5aec2812496425aa52de7a682c4fbef74a38
cabeywic <charaka@basilurtea.com>
2023-04-24 20:26:48 +0100

    Added Cache

--- knowledge_base_search/knowledge_base.py
+++ knowledge_base_search/knowledge_base.py
@@ -1,17 +1,52 @@
 from knowledge_base_search.preprocessing.text_processing import TextProcessor
 from knowledge_base_search.utils.helpers import load_documents
 from knowledge_base_search import logger
+import time 
+import os
+import pickle
+
 
 class KnowledgeBase:
-    def __init__(self, search_algorithm, documents_path):
+    def __init__(self, search_algorithm, documents_path, cache=False):
         self.documents = load_documents(documents_path)
-        self.text_processor = TextProcessor(documents=[doc.content for doc in self.documents])
+        logger.info(f"Loaded {len(self.documents)} documents.")
 
-        processed_documents = [' '.join(self.text_processor.process_text(doc.content)) for doc in self.documents]
+        search_algorithm_cache_filename = f"{type(search_algorithm).__name__}_search_algorithm.pickle"
+        text_processor_cache_filename = "text_processor.pickle"
+        print(os.path.exists(search_algorithm_cache_filename), os.path.exists(text_processor_cache_filename))
 
-        self.search_algorithm = search_algorithm
-        self.search_algorithm.index_documents(processed_documents)
-        logger.info(f"Set search algorithm to {type(search_algorithm).__name__}.")
+        if cache and os.path.exists(search_algorithm_cache_filename) and os.path.exists(text_processor_cache_filename):
+            os.environ["TOKENIZERS_PARALLELISM"] = "false"
+            
+            with open(search_algorithm_cache_filename, "rb") as f:
+                self.search_algorithm = pickle.load(f)
+
+            with open(text_processor_cache_filename, "rb") as f:
+                self.text_processor = pickle.load(f)
+
+            logger.info("Loaded search_algorithm and text_processor from cache.")
+        else:
+            start_time = time.time()
+            self.text_processor = TextProcessor(documents=[doc.content for doc in self.documents])
+            if cache:
+                with open(text_processor_cache_filename, "wb") as f:
+                    pickle.dump(self.text_processor, f)
+                logger.info("Saved text_processor to cache.")
+
+            processed_documents = [' '.join(self.text_processor.process_text(doc.content)) for doc in self.documents]
+            print(f"Time to process documents: {time.time() - start_time} seconds")
+
+            self.search_algorithm = search_algorithm
+            self.search_algorithm.index_documents(processed_documents)
+
+            if cache:
+                with open(search_algorithm_cache_filename, "wb") as f:
+                    pickle.dump(self.search_algorithm, f)
+                logger.info("Saved search_algorithm to cache.")
+
+            print(f"Time to index documents: {time.time() - start_time} seconds")
+            logger.info(f"Set search algorithm to {type(search_algorithm).__name__}.")
+
 
     def search(self, query, top_n=5):
         processed_query = ' '.join(self.text_processor.process_text(query))
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from knowledge_base_search.knowledge_base import KnowledgeBase
 from knowledge_base_search.answer_generator import AnswerGenerator
-from knowledge_base_search.search.bert_search import BERTSearch
+from knowledge_base_search.search.minilm_search import MiniLMSearch
 
 # Example search query
 query = "Who is the CEO and the Managing Director?"
 
 # Load documents
-knowledge_base_bert = KnowledgeBase(BERTSearch(), "data/raw_data/documents.json")
+knowledge_base_bert = KnowledgeBase(MiniLMSearch(), "data/raw_data/documents.json", cache=True)
 
 # Search using BERTSearch
 top_n = 2
@@ -14,7 +14,7 @@ top_documents_bert = knowledge_base_bert.search(query, top_n)
 print(f"Top {top_n} documents for the query using BERTSearch:", top_documents_bert)
 
 # Set your OpenAI API key
-openai_api_key = "<openAI_api_key>"
+openai_api_key = ""
 
 # Create an AnswerGenerator instance
 answer_generator = AnswerGenerator(openai_api_key)
@@ -22,4 +22,4 @@ answer_generator = AnswerGenerator(openai_api_key)
 # Generate an answer using the most relevant document found by BERTSearch
 top_document = top_documents_bert[0]
 answer = answer_generator.generate_answer(query, top_document)
-print("Generated answer:", answer)
+print("\n\n\nGenerated answer:", answer)
